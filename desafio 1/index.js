a4a4c69f5b7e9d433d7ea709d47e74fdf81622e7
Murilo <murilor393@gmail.com>
2025-10-21 21:04:15 -0300

    Desafio

---
+++ desafio 1/index.js
@@ -0,0 +1,41 @@
+function calcularimc() {
+    const peso = (document.getElementById('peso').value);
+    const altura = (document.getElementById('altura').value);
+    const resultado = document.getElementById('resultado');
+}
+
+const imc = peso / (altura * altura);
+let classificacao = "";
+
+if (imc < 18.5) {
+    classificacao = "Abaixo do peso";
+} else if (imc < 24.9) {
+    classificacao = "Peso normal";
+} else if (imc < 29.9) {
+    classificacao = "Sobrepeso";
+} else if (imc < 34.9) {
+    classificacao = "Obesidade grau I";
+} else if (imc < 39.9) {
+    classificacao = "Obesidade grau II";
+} else (imc < 40); {
+    classificacao = "Obesidade grau III";
+}
+resultado.textContent = "Seu IMC é ${imc.toFixed(2)} (${classificacao})";
+
+const idade = (document.getElementById('idade').value); {
+    if (idade <= 12) {
+        faixaetaria = "Criança";
+    }
+    else if (idade => 13 && idade <= 17) {
+        faixaetaria = "Adolescente";
+    }
+    else if (idade => 18 && idade <= 59) {
+        faixaetaria = "Adulto";
+    }
+    else if (idade => 60 && idade <= 99) {
+        faixaetaria = "Idoso";
+    }
+}
+const linhaidade = document.createElement("p");
+linhaidade.textContent = `${idade}`;
+document.getElementById('linhaidade').appendChild(idade);
